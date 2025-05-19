69c2455497e0418c64892e9f48a4c0135a9af1d4
EnrikoRP <enrikoraina@gmail.com>
2025-05-19 10:37:31 +0700

    feat: implement API for XYZ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,29 @@ android {
 }
 
 dependencies {
-
+    // Library dari libs.versions.toml
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Retrofit & Gson (API)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // RecyclerView (for displaying lists)
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+
+    // Glide (for displaying images)
+    implementation("com.github.bumptech.glide:glide:4.12.0")  // Updated version to 4.12.0
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")  // Use the same version for the compiler
+
+    // CardView (for displaying card-based UI)
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.squareup.picasso:picasso:2.8")
 }
---
+++ app/src/main/java/com/example/tryoutpas_absen12_absen30/TeamResponse.java
@@ -0,0 +1,18 @@
+package com.example.tryoutpas_absen12_absen30;
+import java.util.List;
+
+public class TeamResponse {
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+}
+
+
+
+
