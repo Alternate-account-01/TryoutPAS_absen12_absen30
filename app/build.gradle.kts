d34090ee59fb3d59f3cb484533ca1f90ac4fdef2
Alternate-account-01 <Dinata.Rafan@gmail.com>
2025-05-19 12:32:43 +0700

    bismillah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.tryoutpas_absen12_absen30"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tryoutpas_absen12_absen30"
